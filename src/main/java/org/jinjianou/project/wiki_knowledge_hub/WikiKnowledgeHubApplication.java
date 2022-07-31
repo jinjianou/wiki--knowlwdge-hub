aabe98a73ea37456882d75ca938dee6f8fa18648
jinjianou3 <jinjianou@163.com>
2022-07-31 13:10:33 +0800

    1-创建项目

---
+++ src/main/java/org/jinjianou/project/wiki_knowledge_hub/WikiKnowledgeHubApplication.java
@@ -0,0 +1,13 @@
+package org.jinjianou.project.wiki_knowledge_hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WikiKnowledgeHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WikiKnowledgeHubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/jinjianou/project/wiki_knowledge_hub/WikiKnowledgeHubApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jinjianou.project.wiki_knowledge_hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WikiKnowledgeHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
